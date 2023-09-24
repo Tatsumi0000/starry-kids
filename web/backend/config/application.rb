a193aaa3e40152f9f81863acf799cab552c47a40
Tatsumi0000 <datsunote0211@gmail.com>
2023-09-24 18:15:10 +0900

    rails g で動いてほしくないものの設定をした
    https://zenn.dev/tmasuyama1114/books/ab51fea5d5f659/viewer/rspec-introduction#%E5%88%9D%E6%9C%9F%E8%A8%AD%E5%AE%9A これを参考にした

--- web/backend/config/application.rb
+++ web/backend/config/application.rb
@@ -1,6 +1,6 @@
-require_relative "boot"
+require_relative 'boot'
 
-require "rails/all"
+require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -10,6 +10,15 @@ module Backend
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
+
+    config.generators do |g|
+      g.assets false
+      g.helper false
+      g.test_framework :rspec,
+                       fixtures: false,
+                       view_specs: false, # ビューファイル用のスペックを作成しない
+                       helper_specs: false # ヘルパーファイル用のスペックを作成しない
+    end
 
     # Configuration for the application, engines, and railties goes here.
     #
