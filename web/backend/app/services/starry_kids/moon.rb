ef987d0a5d5441af796c9f4bad9b1deba2eb95f5
Tatsumi0000 <datsunote0211@gmail.com>
2023-09-25 23:22:26 +0900

    月の情報を持つMoonクラスを定義

---
+++ web/backend/app/services/starry_kids/moon.rb
@@ -0,0 +1,78 @@
+require 'numo/narray'
+
+module StarryKids
+  class Moon
+    class << self
+      def emoji
+        # 🌑
+        moon0 = Numo::Int32.cast([[-1, -1, -1, -1],
+                                  [-1, -1, -1, -1],
+                                  [-1, -1, -1, -1],
+                                  [-1, -1, -1, -1]])
+        # 🌒
+        moon1 = Numo::Int32.cast([[-1, -1, -1, 1],
+                                  [-1, -1, -1, 1],
+                                  [-1, -1, -1, 1],
+                                  [-1, -1, -1, 1]])
+        # 🌓
+        moon2 = Numo::Int32.cast([[-1, -1, 1, 1],
+                                  [-1, -1, 1, 1],
+                                  [-1, -1, 1, 1],
+                                  [-1, -1, 1, 1]])
+        # 🌔
+        moon3 = Numo::Int32.cast([[-1, 1, 1, 1],
+                                  [-1, 1, 1, 1],
+                                  [-1, 1, 1, 1],
+                                  [-1, 1, 1, 1]])
+        # 🌘
+        moon4 = Numo::Int32.cast([[1, -1, -1, -1],
+                                  [1, -1, -1, -1],
+                                  [1, -1, -1, -1],
+                                  [1, -1, -1, -1]])
+        # 🌗
+        moon5 = Numo::Int32.cast([[1, 1, -1, -1],
+                                  [1, 1, -1, -1],
+                                  [1, 1, -1, -1],
+                                  [1, 1, -1, -1]])
+        # 🌖
+        moon6 = Numo::Int32.cast([[1, 1, 1, -1],
+                                  [1, 1, 1, -1],
+                                  [1, 1, 1, -1],
+                                  [1, 1, 1, -1]])
+        # 🌕
+        moon7 = Numo::Int32.cast([[1, 1, 1, 1],
+                                  [1, 1, 1, 1],
+                                  [1, 1, 1, 1],
+                                  [1, 1, 1, 1]])
+
+        [moon0, moon1, moon2, moon3, moon4, moon5, moon6, moon7]
+      end
+
+      def convert_to_emoji(index)
+        case index
+        when 0
+          '🌑'
+        when 1
+          '🌒'
+        when 2
+          '🌓'
+        when 3
+          '🌔'
+        when 4
+          '🌘'
+        when 5
+          '🌗'
+        when 6
+          '🌖'
+        when 7
+          '🌕'
+        end
+      end
+    end
+  end
+end
+
+if $0 == __FILE__
+  moon = StarryKids::Moon
+  p moon.emoji
+end
