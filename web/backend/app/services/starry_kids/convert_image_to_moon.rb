af29a08ad7a544f6873155690ca562f7a3f21d32
Tatsumi0000 <datsunote0211@gmail.com>
2023-09-28 00:21:15 +0900

    正規化するメソッドを生やした

--- web/backend/app/services/starry_kids/convert_image_to_moon.rb
+++ web/backend/app/services/starry_kids/convert_image_to_moon.rb
@@ -1,7 +1,13 @@
+require 'numo/narray'
+
 module StarryKids
   class ConvertImageToMoon
     class << self
-
+      
+      # -1 ~ 1.0の範囲に正規化する
+      def normalize(array)
+        (Numo::Int32.cast(array) / 128.0) - 1.0
+      end
     end
 
   end
