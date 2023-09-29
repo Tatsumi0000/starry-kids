c430ba13d219d15e0077e2ec15bac91da4b1285f
Tatsumi0000 <datsunote0211@gmail.com>
2023-09-30 05:38:26 +0900

    コメントを書いた

--- web/backend/app/services/starry_kids/convert_image_to_moon.rb
+++ web/backend/app/services/starry_kids/convert_image_to_moon.rb
@@ -8,20 +8,25 @@ module StarryKids
       @convert_character_to_image = StarryKids::ConvertCharacterToImage.new
     end
 
+    # 文字→画像→月文字に変換する
+    # @param [String] character 月文字にしたい1文字
     def call(character)
+      # 月文字を入れる配列
       moon_array = []
       @convert_character_to_image.generate_text_image(character)
       gray_scale_image = @convert_character_to_image.convert_to_graysclae_pixels
       normalize = ConvertCharacterToImage.normalize(gray_scale_image)
+      # 行数(shape[0])と列数(shape[1])を 1 / 4 した分だけ計算する。
+      # 1 / 4 するのは4 * 4ピクセルを1文字の月で表現するため。
       (normalize.shape[0] / 4).times do |i|
         (normalize.shape[1] / 4).times do |j|
           row = 4 * i
           col = 4 * j
-          max = -10_000
+          max = -100
           max_index = 0
 
           StarryKids::Moon.emoji.each_with_index do |moon, k|
-            # 4*4の行列を作って、月絵文字とのアダマール積を計算
+            # 4*4の行列を抜き出して、月絵文字とのアダマール積を計算
             hadamard_product = normalize.slice(row...(row + 4), col...(col + 4)) * moon
             if hadamard_product.sum > max
               max_index = k
