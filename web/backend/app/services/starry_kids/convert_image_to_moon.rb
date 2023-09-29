6735ddb047c1f6a7088e8d429dcb18569fbb0ea7
Tatsumi0000 <datsunote0211@gmail.com>
2023-09-30 03:16:29 +0900

    一通り月文字に変換する処理を書いたけどなんかおかしい

--- web/backend/app/services/starry_kids/convert_image_to_moon.rb
+++ web/backend/app/services/starry_kids/convert_image_to_moon.rb
@@ -1,18 +1,44 @@
 require 'numo/narray'
-require_ralative 'convert_character_to_image'
+require_relative 'moon'
+require_relative 'convert_character_to_image'
 
 module StarryKids
   class ConvertImageToMoon
-    class << self
-      
-      private
-
-      # -1 ~ 1.0の範囲に正規化する
-      def normalize(array)
-        (Numo::Int32.cast(array) / 128.0) - 1.0
-      end
+    def initialize
+      @convert_character_to_image = StarryKids::ConvertCharacterToImage.new
     end
 
+    def call(character)
+      moon_array = []
+      @convert_character_to_image.generate_text_image(character)
+      gray_scale_image = @convert_character_to_image.convert_to_graysclae_pixels
+      normalize = ConvertCharacterToImage.normalize(gray_scale_image)
+      (normalize.shape[0] / 4).times do |i|
+        (normalize.shape[1] / 4).times do |j|
+          row = 4 * i
+          col = 4 * j
+          max = -10_000
 
+          StarryKids::Moon.emoji.each_with_index do |moon, k|
+            # 4*4の行列を作って、月絵文字とのアダマール積を計算
+            hadamard_product = normalize.slice(row...(row + 4), col...(col + 4)) * moon
+            if hadamard_product.sum > max
+              max_index = k
+              max = hadamard_product.sum
+            end
+            moon_array << StarryKids::Moon.convert_to_emoji(max_index)
+          end
+          moon_array << "\n"
+        end
+      end
+      moon_array.each do |emoji|
+        print emoji
+      end
+    end
   end
+end
+
+if $0 == __FILE__
+  convert_image_to_moon = StarryKids::ConvertImageToMoon.new
+  convert_image_to_moon.call('A')
 end
