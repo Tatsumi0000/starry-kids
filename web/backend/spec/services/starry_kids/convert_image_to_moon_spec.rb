e9f758dd58289cb065ec747ebcd9aa57bc15df08
Tatsumi0000 <datsunote0211@gmail.com>
2023-10-01 02:48:54 +0900

    ConvertImageToMoonServiceのテストを書いた

--- web/backend/spec/services/starry_kids/convert_image_to_moon_spec.rb
+++ web/backend/spec/services/starry_kids/convert_image_to_moon_spec.rb
@@ -1,3 +1,25 @@
 # frozen_string_literal: true
 
 require 'rails_helper'
+
+RSpec.describe StarryKids::ConvertImageToMoonService, type: :service do 
+
+  
+en  let!(:service) { StarryKids::ConvertImageToMoonService.new(80) }
+    context '文字が「あ」のとき' do
+      let!(:moon) { service.call('あ') }
+
+      it '左上が「🌕」'であることを確認 do
+        expect(moon.chars.first).to eq '🌕'
+      end
+
+      it '総文字数が420文字であることを確認' do
+        # 80x80の画像は4x4の範囲を1つの月として表現するので、
+        # 80 / 4 = 20。それを縦横なので 20 * 20 = 400になる
+        # さらに改行文字が20個（高さの分）あるので 400 + 20 = 420
+        expect(moon.length).to eq 420
+      end
+    end
+  end
+
+  
