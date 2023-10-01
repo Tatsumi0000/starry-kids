7b3b10e4037b7c0caf98cfaf49843ccc7988954b
Tatsumi0000 <datsunote0211@gmail.com>
2023-10-01 21:54:06 +0900

    なぜかシングルクォーテーションがずれてたので修正

--- web/backend/spec/services/starry_kids/convert_image_to_moon_spec.rb
+++ web/backend/spec/services/starry_kids/convert_image_to_moon_spec.rb
@@ -2,24 +2,21 @@
 
 require 'rails_helper'
 
-RSpec.describe StarryKids::ConvertImageToMoonService, type: :service do 
+RSpec.describe StarryKids::ConvertImageToMoonService, type: :service do
+  let!(:service) { StarryKids::ConvertImageToMoonService.new(80) }
 
-  
-en  let!(:service) { StarryKids::ConvertImageToMoonService.new(80) }
-    context '文字が「あ」のとき' do
-      let!(:moon) { service.call('あ') }
+  context '文字が「あ」のとき' do
+    let!(:moon) { service.call('あ') }
 
-      it '左上が「🌕」'であることを確認 do
-        expect(moon.chars.first).to eq '🌕'
-      end
+    it '左上が「🌕」であることを確認' do
+      expect(moon[0]).to eq '🌕'
+    end
 
-      it '総文字数が420文字であることを確認' do
-        # 80x80の画像は4x4の範囲を1つの月として表現するので、
-        # 80 / 4 = 20。それを縦横なので 20 * 20 = 400になる
-        # さらに改行文字が20個（高さの分）あるので 400 + 20 = 420
-        expect(moon.length).to eq 420
-      end
+    it '総文字数が420文字であることを確認' do
+      # 80x80の画像は4x4の範囲を1つの月として表現するので、
+      # 80 / 4 = 20。それを縦横なので 20 * 20 = 400になる
+      # さらに改行文字が20個（高さの分）あるので 400 + 20 = 420
+      expect(moon.length).to eq 420
     end
   end
-
-  
+end
