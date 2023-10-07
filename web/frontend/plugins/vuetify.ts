724c8563219be355c61ba8d1412ab049c31fac4f
Tatsumi0000 <datsunote0211@gmail.com>
2023-10-08 02:10:46 +0900

    vuetifyの設定を書いた
    - https://vuetifyjs.com/en/getting-started/installation/#existing-projects
    - https://github.com/vuetifyjs/vuetify-loader/tree/master/packages/vite-plugin

---
+++ web/frontend/plugins/vuetify.ts
@@ -0,0 +1,11 @@
+import "vuetify/styles";
+import { createVuetify } from "vuetify";
+import * as components from "vuetify/components";
+import * as directives from "vuetify/directives";
+
+const vuetify = createVuetify({
+  components,
+  directives,
+});
+
+export default vuetify;
