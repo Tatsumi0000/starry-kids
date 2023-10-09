c4bf185a1992fec5d202eb9903501f78b41ca928
Tatsumi0000 <datsunote0211@gmail.com>
2023-10-09 17:45:01 +0900

    自動整形とlinterを動かせるようにした
    そして整形した。
    - npm run format
    - npm run lint
    
    で整形とリンターが動く

--- web/frontend/vite.config.ts
+++ web/frontend/vite.config.ts
@@ -5,10 +5,7 @@ import vuetify from "vite-plugin-vuetify";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vuetify({ autoImport: true }),
-  ],
+  plugins: [vue(), vuetify({ autoImport: true })],
   server: {
     host: true,
   },
