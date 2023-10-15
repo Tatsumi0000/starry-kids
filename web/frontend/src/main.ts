b415d033eb12d15db4dca34fa479fa6f0aa7811b
Tatsumi0000 <datsunote0211@gmail.com>
2023-10-15 23:29:07 +0900

    vue-routerとvuetifyの設定をした

--- web/frontend/src/main.ts
+++ web/frontend/src/main.ts
@@ -1,5 +1,9 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import vuetify from "../plugins/vuetify";
+import router from "./configs/router";
 
-createApp(App).use(vuetify).mount("#app");
+const app = createApp(App);
+app.use(vuetify);
+app.use(router);
+app.mount("#app");
