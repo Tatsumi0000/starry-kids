c6f56ad70538c3b827aa5d55513b3d66c8aacb3c
Tatsumi0000 <datsunote0211@gmail.com>
2023-11-15 00:57:05 +0900

    フッターを追加

---
+++ web/frontend/src/components/TheFooter.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+import myIcon from "../assets/tatsumi0000.webp";
+import { ref } from "vue";
+
+const icon = ref(myIcon);
+const openWindow = (url: string) => {
+  window.open(url, "_blank");
+};
+</script>
+
+<template>
+  <v-container>
+    <v-footer app class="bg-indigo-lighten-1 flex-column">
+      <v-btn
+        class="ma-2"
+        color="primary"
+        @click="openWindow('https://github.com/Tatsumi0000/starry-kids/')"
+      >
+        <v-icon start icon="mdi-github"></v-icon>
+        <span>GitHub</span>
+      </v-btn>
+      <v-row>
+        <v-col cols="2">
+          <v-img :src="icon" height="40" width="40" />
+        </v-col>
+        <v-col cols="10">
+          Copyright © {{ new Date().getFullYear() }}, Tatsumi0000 All Rights Reserved.
+        </v-col>
+      </v-row>
+    </v-footer>
+  </v-container>
+</template>
+
+<style scoped lang="scss"></style>
