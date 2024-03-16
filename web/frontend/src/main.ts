e59110cfc9398ffb294e35ad1e949f632d414435
Tatsumi0000 <19218690+Tatsumi0000@users.noreply.github.com>
2024-03-16 20:41:38 +0900

    vue-gtagの初期化を行って計測するようにします

--- web/frontend/src/main.ts
+++ web/frontend/src/main.ts
@@ -1,5 +1,6 @@
 import { createApp } from "vue";
 import App from "./App.vue";
+import VueGtag from "vue-gtag";
 import vuetify from "../plugins/vuetify";
 import router from "./configs/router";
 import "@mdi/font/css/materialdesignicons.css";
@@ -7,4 +8,7 @@ import "@mdi/font/css/materialdesignicons.css";
 const app = createApp(App);
 app.use(vuetify);
 app.use(router);
+app.use(VueGtag, {
+    config: { id: "G-9VS0SV5JBW" }
+});
 app.mount("#app");
