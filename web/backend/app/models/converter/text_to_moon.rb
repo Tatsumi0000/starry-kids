dbcfe889ed6d243e9595033ecb07307a01fd4198
Tatsumi0000 <19218690+Tatsumi0000@users.noreply.github.com>
2024-04-01 20:42:59 +0900

    漢字対応にしたいのでバリデーションを外す
    - https://github.com/Tatsumi0000/starry-kids/issues/196
    
    これの影響で日本語以外（韓国語とか中国語など）のバリデーションも外れるけど特に問題ないので大丈夫です

--- web/backend/app/models/converter/text_to_moon.rb
+++ web/backend/app/models/converter/text_to_moon.rb
@@ -12,7 +12,6 @@ module Converter
 
     validates :text, presence: true
     validates :text, length: { minimum: 1, maximum: 20 }
-    validates :text, format: { with: /\A[ぁ-んァ-ンa-zA-Z0-9ー０-９Ａ-Ｚａ-ｚ 　]+\z/, message: '変換できるのは全角半角英数字、ひらがなカタカナのみです。' }
     validates :size, numericality: { only_integer: true, greater_than_or_equal_to: 10, less_than_or_equal_to: 100 }
 
     def initialize(params = {})
