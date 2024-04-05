ef0c79f87aa60f2d5ede59f135a7db02a012b018
Tatsumi0000 <19218690+Tatsumi0000@users.noreply.github.com>
2024-04-06 03:00:05 +0900

    canvas内では改行コードを認識してくれないのでそれを考慮した設計に修正

--- web/frontend/src/components/DownloadButton.vue
+++ web/frontend/src/components/DownloadButton.vue
@@ -9,11 +9,22 @@ const props = defineProps<{
 const convertTextTiImage = () => {
   const canvas = document.createElement("canvas");
   const ctx = canvas.getContext("2d")!;
+  // 改行コードで分割
+  const lines = props.text.split("\n");
 
   ctx.font = props.size.toString + "px serif";
-  canvas.width = ctx.measureText(props.text).width;
-  canvas.height = ctx.measureText(props.text).width * props.text.length;
-  ctx.fillText(props.text, 0, props.size);
+  // 1行あたりの高さを取得
+  const lineHeight =
+    ctx.measureText(props.text).actualBoundingBoxAscent +
+    ctx.measureText(props.text).actualBoundingBoxDescent;
+
+  canvas.width = ctx.measureText(props.text.split("\n")[0]).width;
+  canvas.height = lineHeight * lines.length;
+
+  // canvas要素では改行を認識してくれないので「/n」で分割して1行ずつ描画
+  lines.map((line, index) => {
+    ctx.fillText(line, 0, 0 + (index + 1) * lineHeight);
+  });
 
   const imageDataURL = canvas.toDataURL("image/png");
   const link = document.createElement("a");
